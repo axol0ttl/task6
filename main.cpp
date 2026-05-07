d71b215da7c50b46cd86d655d18e8f6afc710acf
Arseny Holodov <holodov.aa@edu.spbstu.ru>
2026-05-07 14:12:13 +0300

    lots of little changes

--- main.cpp
+++ main.cpp
@@ -7,14 +7,13 @@
 #include "objects/MOline.h"
 #include "objects/MOarc.h"
 
-//TODO исправить: убрать переменные в h файлах (я пока не знаю че еще сказать)
 
 void Main(void) {
     const int num = 11;
     object* list[num];
     float n = 4.5;
 
-    // 1. Создаем разные объекты
+    //Создаем разные объекты
 
     list[0] = new    MOline(400*n, 100*n, 0*n, 0*n, 0*n, 100*n, 255, 0, 0);
     list[1] = new    MOline(400*n, 100*n, 0*n, 0*n, 100*n, 0*n, 255, 0, 0);
@@ -25,48 +24,56 @@ void Main(void) {
     list[6] = new    MOarc(450*n, 150*n, 25*n, 0, 360, 255, 123, 123);
     list[7] = new    MOsquare(450*n, 150*n, 50*n, 100*n, 255, 0, 255); // квадрат настоящий
     list[8] = new    MOsector(100*n, 150*n, 100*n, 30, 67, 0*n, 100*n, 255, 255, 0);
-    list[9] = new    MOsegment(300*n, 150*n, 50*n, 45, 135, 255, 0, 255); // сегмент (дуга + хорда)
+    list[9] = new    MOsegment(300*n, 150*n, 50*n, 45, 135, 255, 0, 255); // сегмент (дуга + арка)
     list[10]= new    MNumber(555);
 
-    // 2. Демонстрируем полиморфизм и dynamic_cast
+    wait4keyORmouse();
+
+    // Демонстрируем полиморфизм и dynamic_cast
     for (int i = 0; i < num; i++) {
         std::cout << "Объект " << i << ": ";
         list[i]->print(); // Виртуальный метод печати
         std::cout << std::endl;
         // Пытаемся привести к фигуре, чтобы подвинуть/нарисовать
-        figure_selfmade* fig = dynamic_cast<figure_selfmade*>(list[i]);
-
-        wait4keyORmouse();
-        if (fig) { // если фигура, сдвигаем
-
-            fig->move(20*n, 20*n); // Метод фигуры
+        try {
+            figure_selfmade* fig = dynamic_cast<figure_selfmade*>(list[i]);
 
 
-        } else {
-            // Это не фигура, пропускаем сдвиг
+
+            if (fig) { // если фигура, сдвигаем
+                fig->move(50*n, 20*n); // Метод фигуры
+            }
+            else {
+                // Это не фигура, пропускаем сдвиг
+            }
         }
-
+        catch (const std::bad_cast& e) {
+            std::cerr << "ошибка приведения" << std::endl;
+        }
     }
     wait4keyORmouse();
 
-    // 3. Пример со ссылкой (на оценку 5+)
     try {
         std::cout << "\nПроверка первого элемента на число..." << std::endl;
         MNumber& numRef = dynamic_cast<MNumber&>(*list[0]);
         numRef.print();
     } catch (const std::bad_cast& e) {
-        std::cout << "Ошибка приведения!" << std::endl;
+        std::cerr << "Ошибка приведения!" << std::endl;
     }
 
     // Чистка
     for (int i = 0; i < num; i++) {
         // Если это фигура, сотрем с экрана перед удалением
-        figure_selfmade* fig = dynamic_cast<figure_selfmade*>(list[i]);
-        if (fig) fig->erase();
+        try {
+            figure_selfmade* fig = dynamic_cast<figure_selfmade*>(list[i]);
+            if (fig) fig->erase();
 
-        delete list[i];
+            delete list[i];
+        }
+        catch (const std::bad_cast& e) {
+            std::cerr << "ошибка приведения" << std::endl;
+        }
     }
-
-    std::cout << "Готово!" << std::endl;
     wait4keyORmouse();
+
 }
