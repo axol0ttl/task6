b3600bf074a5ce693aa55c90c0ae334477a5e6a8
Arseny Holodov <holodov.aa@edu.spbstu.ru>
2026-05-12 15:37:38 +0300

    hided structure inside object.h

--- main.cpp
+++ main.cpp
@@ -7,59 +7,33 @@
 #include "objects/MOline.h"
 #include "objects/MOarc.h"
 
-// Структура узла двусвязного списка
-struct Node {
-    object* data;    // Указатель на объект (линия, сектор и т.д.)
-    Node* pNext;     // Ссылка на следующий элемент
-    Node* pPrev;     // Ссылка на предыдущий элемент
-};
-
-// Глобальные указатели списка
-Node* pFirst = nullptr;
-Node* pLast = nullptr;
-
-// Функция добавления объекта в список
-void AddToList(object* obj) {
-    if (!obj) return;
-
-    Node* pNew = new Node;
-    pNew->data = obj;
-    pNew->pNext = nullptr;
-    pNew->pPrev = pLast;
-
-    if (pLast) {
-        pLast->pNext = pNew;
-    }
-    else {
-        pFirst = pNew;
-    }
-    pLast = pNew;
-}
-
 void Main(void) {
     float n = 4.5;
 
-    // 1. Создание объектов и добавление их в двусвязный список
-    AddToList(new MOline(400*n, 100*n, 0*n, 0*n, 0*n, 100*n, 255, 0, 0));
-    AddToList(new MOline(400*n, 100*n, 0*n, 0*n, 100*n, 0*n, 255, 0, 0));
-    AddToList(new MOline(400*n, 200*n, 0*n, 0*n, 100*n, 0*n, 255, 0, 0));
-    AddToList(new MOline(500*n, 100*n, 0*n, 0*n, 0*n, 100*n, 255, 0, 0));
-    AddToList(new MOline(400*n, 200*n, 0*n, 0*n, 50*n, 50*n, 255, 0, 0));
-    AddToList(new MOline(500*n, 200*n, 0*n, 0*n, -50*n, 50*n, 255, 0, 0));
-    AddToList(new MOarc(450*n, 150*n, 25*n, 0, 360, 255, 123, 123));
-    AddToList(new MOsquare(450*n, 150*n, 50*n, 100*n, 255, 0, 255));
-    AddToList(new MOsector(100*n, 150*n, 100*n, 30, 67, 0*n, 100*n, 255, 255, 0));
-    AddToList(new MOsegment(300*n, 150*n, 50*n, 45, 135, 255, 0, 255));
-    AddToList(new MNumber(555));
+    // Создаем контейнер для управления объектами
+    ObjectList objects;
+
+    // Добавляем объекты в список
+    objects.add(new MOline(400*n, 100*n, 0*n, 0*n, 0*n, 100*n, 255, 0, 0));
+    objects.add(new MOline(400*n, 100*n, 0*n, 0*n, 100*n, 0*n, 255, 0, 0));
+    objects.add(new MOline(400*n, 200*n, 0*n, 0*n, 100*n, 0*n, 255, 0, 0));
+    objects.add(new MOline(500*n, 100*n, 0*n, 0*n, 0*n, 100*n, 255, 0, 0));
+    objects.add(new MOline(400*n, 200*n, 0*n, 0*n, 50*n, 50*n, 255, 0, 0));
+    objects.add(new MOline(500*n, 200*n, 0*n, 0*n, -50*n, 50*n, 255, 0, 0));
+    objects.add(new MOarc(450*n, 150*n, 25*n, 0, 360, 255, 123, 123));
+    objects.add(new MOsquare(450*n, 150*n, 50*n, 100*n, 255, 0, 255));
+    objects.add(new MOsector(100*n, 150*n, 100*n, 30, 67, 0*n, 100*n, 255, 255, 0));
+    objects.add(new MOsegment(300*n, 150*n, 50*n, 45, 135, 255, 0, 255));
+    objects.add(new MNumber(555));
 
     wait4keyORmouse();
 
-    // 2. Проход по списку: Печать и Движение
+    // Проход по списку: Печать и Движение
     std::cout << "обработка списка объектов" << std::endl;
-    Node* pCurrent = pFirst;
+    Node* pCurrent = objects.getFirst();
     int index = 0;
     while (pCurrent != nullptr) {
-        std::cout << "Узел " << index++ << ": ";
+        std::cout << index++ << ": ";
 
         // Полиморфный вызов печати
         pCurrent->data->print();
@@ -76,38 +50,32 @@ void Main(void) {
 
     wait4keyORmouse();
 
-    // 3. Пример проверки конкретного типа (dynamic_cast для первого элемента)
+    // Проверка конкретного типа (dynamic_cast для первого элемента)
+    Node* pFirst = objects.getFirst();
     if (pFirst && pFirst->data) {
-        std::cout << "Проверка первого элемента на тип MNumber..." << std::endl;
+        std::cout << "проверка первого элемента на тип MNumber..." << std::endl;
         MNumber* isNum = dynamic_cast<MNumber*>(pFirst->data);
         if (isNum) {
             isNum->print();
         }
         else {
-            std::cout << "Первый элемент не является числом." << std::endl;
+            std::cout << "первый элемент не является числом" << std::endl;
         }
     }
 
-    // 4. Очистка памяти: удаление объектов, стирание с экрана и удаление узлов
+    // Очистка памяти: удаление объектов, стирание с экрана
     std::cout << "очистка памяти и удаление объектов" << std::endl;
-    pCurrent = pFirst;
+    pCurrent = objects.getFirst();
     while (pCurrent != nullptr) {
         // Стираем фигуру перед удалением
         figure_selfmade* fig = dynamic_cast<figure_selfmade*>(pCurrent->data);
         if (fig) fig->erase();
 
-        // Удаляем сам объект
-        delete pCurrent->data;
-
-        // Удаляем текущий узел и переходим к следующему
-        Node* pTemp = pCurrent;
         pCurrent = pCurrent->pNext;
-        delete pTemp;
     }
 
-    // Сбрасываем указатели
-    pFirst = pLast = nullptr;
+    // ObjectList автоматически удалит все узлы в деструкторе
+    objects.deleteObjects();
 
-    std::cout << "Готово. Все объекты и узлы удалены." << std::endl;
     wait4keyORmouse();
 }
--- objects/object.h
+++ objects/object.h
@@ -4,14 +4,14 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <memory>
 
 class object {
 public:
     virtual ~object() {}
-    virtual void print(const std::string& suffix = "") const{
+    virtual void print(const std::string& suffix = "") const {
         std::cout << "я объект " << suffix << std::endl;
     }
-
 };
 
 class MNumber : public object {
@@ -24,4 +24,65 @@ public:
     }
 };
 
+// Вспомогательные структуры для внутреннего управления списком
+struct Node {
+    object* data;
+    Node* pNext;
+    Node* pPrev;
 
+    Node(object* obj) : data(obj), pNext(nullptr), pPrev(nullptr) {}
+};
+
+// Контейнер для управления двусвязным списком объектов
+class ObjectList {
+private:
+    Node* pFirst;
+    Node* pLast;
+
+public:
+    ObjectList() : pFirst(nullptr), pLast(nullptr) {}
+
+    ~ObjectList() {
+        clear();
+    }
+
+    // Добавить объект в конец списка
+    void add(object* obj) {
+        if (!obj) return;
+
+        Node* pNew = new Node(obj);
+        pNew->pPrev = pLast;
+
+        if (pLast) {
+            pLast->pNext = pNew;
+        } else {
+            pFirst = pNew;
+        }
+        pLast = pNew;
+    }
+
+    // Получить указатель на первый элемент (для обхода)
+    Node* getFirst() const {
+        return pFirst;
+    }
+
+    // Очистить список (удалить все узлы)
+    void clear() {
+        Node* pCurrent = pFirst;
+        while (pCurrent != nullptr) {
+            Node* pTemp = pCurrent;
+            pCurrent = pCurrent->pNext;
+            delete pTemp;
+        }
+        pFirst = pLast = nullptr;
+    }
+
+    // Удалить объекты (без удаления узлов)
+    void deleteObjects() {
+        Node* pCurrent = pFirst;
+        while (pCurrent != nullptr) {
+            delete pCurrent->data;
+            pCurrent = pCurrent->pNext;
+        }
+    }
+};
